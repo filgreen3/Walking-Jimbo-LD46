950d9dba694850902d3b5d164bea52a962c514fa
fememism <git@koro.games>
2020-04-17 15:01:10 +0300

    Анимации врагов

---
+++ Assets/animation_mets.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation_mets : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Animator>().SetFloat("ASpeed", Random.Range(0.7f, 1.3f));
+    }
+    public void Rotate()
+    {
+        gameObject.transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+
+}
