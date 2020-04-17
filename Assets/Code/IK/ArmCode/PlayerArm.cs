2b1142cd1e238f5c707eb61b5347afa960730059
Grigory Krasnov <51174663+filgreen3@users.noreply.github.com>
2020-04-17 15:59:38 +0300

    spot-can-grab

--- Assets/Code/IK/ArmCode/PlayerArm.cs
+++ Assets/Code/IK/ArmCode/PlayerArm.cs
@@ -3,8 +3,18 @@ using UnityEngine;
 public class PlayerArm : ArmIK
 {
 
+
+
     public Transform Clac1;
     public Transform Clac2;
+
+    [Header("Take Setting")]
+    public Transform RigTakeObj;
+    public Rigidbody2D TakedObject;
+
+    public LayerMask TakeMask;
+
+    public Vector3 prevPoint;
 
     protected override void Update()
     {
@@ -19,7 +29,17 @@ public class PlayerArm : ArmIK
             Clac1.Rotate(Vector3.forward, 180);
             Clac2.Rotate(Vector3.forward, 180);
 
+
+            var t = Physics2D.OverlapCircleAll(RigTakeObj.position, 1f, TakeMask);
+            if (t.Length > 0)
+            {
+                TakedObject = t[0].gameObject.GetComponent<Rigidbody2D>();
+                TakedObject.gravityScale = 0f;
+            }
         }
+
+
+
         if (Input.GetMouseButtonUp(0))
         {
             Clac1.Rotate(Vector3.forward, -180);
@@ -29,7 +49,26 @@ public class PlayerArm : ArmIK
             Clac2.Rotate(Vector3.forward, 130);
 
 
+            if (TakedObject != null)
+            {
+                TakedObject.gravityScale = 1f;
+                TakedObject.velocity = Vector3.ClampMagnitude((RigTakeObj.position - prevPoint) * 50f, 10f);
+                TakedObject = null;
+            }
         }
+
+
+
+        if (TakedObject != null && Input.GetMouseButton(0))
+        {
+            TakedObject.MovePosition(RigTakeObj.position);
+
+            if ((RigTakeObj.position - prevPoint).magnitude>0.1f)
+                prevPoint = RigTakeObj.position;
+
+        }
+
+
 
 
     }
--- Assets/Code/IK/IK2D/LegIK.cs
+++ Assets/Code/IK/IK2D/LegIK.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class LegIK : GenericSubTargetIK
 {
+
     public AnimationCurve curve;
-    private float defVlaue=0.5f;
+    public float Speed;
+    public bool ProcessState;
+
+    [SerializeField] private LegIK ConnectedLeg;
+    private float defVlaue = 0.5f;
 
 
+
+    private Vector2 currPoint;
+    private Vector2 savePoint;
 
 
     private Vector2 Point
     {
         get
         {
-            if (process) return savePoint;
+            if (ProcessState) return savePoint;
 
             var point = Physics2D.Raycast(targetObj.position, Vector2.down).point;
 
-            if (Mathf.Abs( point.x - savePoint.x)> defVlaue)
+            if ((!ConnectedLeg.ProcessState && Mathf.Abs(point.x - savePoint.x) > defVlaue) ||
+            Mathf.Abs(point.x - savePoint.x) > defVlaue * 1.5f)
             {
                 currPoint = point;
+                ProcessState = true;
                 StartCoroutine(processMove());
             }
 
@@ -29,8 +39,6 @@ public class LegIK : GenericSubTargetIK
     }
 
 
-    private Vector2 currPoint;
-    private Vector2 savePoint;
 
 
 
@@ -41,31 +49,28 @@ public class LegIK : GenericSubTargetIK
 
         Bones[0].position = transform.position;
 
-         for (int i = 1; i < Bones.Count; i++)
-             Bones[i].position = Bones[i - 1].endPoint;
+        for (int i = 1; i < Bones.Count; i++)
+            Bones[i].position = Bones[i - 1].endPoint;
     }
 
-    private bool process;
 
-    public float Speed;
 
     private IEnumerator processMove()
     {
-        process = true;
 
-        var waiter=new WaitForFixedUpdate();
+        var waiter = new WaitForFixedUpdate();
         var t = 0f;
 
         while (t < defVlaue)
         {
             savePoint = Vector2.Lerp(savePoint, currPoint, t / defVlaue);
-            savePoint.y += curve.Evaluate(t/defVlaue);
-            t += 0.1f*Speed;
+            savePoint.y += curve.Evaluate(t / defVlaue);
+            t += 0.1f * Speed;
             yield return waiter;
         }
         savePoint = Vector2.Lerp(savePoint, currPoint, t / defVlaue);
-        process = false;
-        defVlaue = Random.value;
+        ProcessState = false;
+        defVlaue = 1 - (Random.value / 20f);
 
 
     }
