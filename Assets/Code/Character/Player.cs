0751f0f3a5aaae2d631056337c326bb0ad34fb35
Grigory Krasnov <51174663+filgreen3@users.noreply.github.com>
2020-04-18 13:42:27 +0300

    new-grab-system

--- Assets/Code/Character/Player.cs
+++ Assets/Code/Character/Player.cs
@@ -7,19 +7,24 @@ public class Player : Character
 
     public virtual Vector3 LookPoint { get => WorldManager.WorldLookPoint; }
     public ArmIK Arm;
+    private Rigidbody2D armTarget;
+    public float ArmForce;
+
 
     private void Start()
     {
-        ArmTarget =   Arm.targetObj.GetComponent<Rigidbody2D>();
+        armTarget = Arm.targetObj.GetComponent<Rigidbody2D>();
     }
 
 
-    public Rigidbody2D ArmTarget;
 
 
     protected override void FixedUpdate()
     {
         base.FixedUpdate();
-        Arm.targetObj.localPosition = Vector3.ClampMagnitude((LookPoint - Arm.transform.position), 5f);
+        var t = Vector2.ClampMagnitude(LookPoint - Arm.transform.position, 3f) + (Vector2)Arm.transform.position;
+        t = (t - armTarget.position) * ArmForce;
+        armTarget.AddForce(t * t.magnitude);
+        Arm.targetObj.localPosition = Vector3.ClampMagnitude(Arm.targetObj.localPosition, 3f);
     }
 }
