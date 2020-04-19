cee32994698be71d4e0a6c0d1673e9f154b8e0d4
fememism <git@koro.games>
2020-04-19 21:10:46 +0300

    tutor1

--- Assets/Door_default.cs
+++ Assets/Door_default.cs
@@ -6,24 +6,31 @@ public class Door_default : MonoBehaviour
 {
     public GameObject Door1;
     public GameObject Door2;
-    bool touched = false;
+    public bool touched = false;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
 
-        if (!touched&&collision.gameObject.tag == "Player") 
+        if (collision.gameObject.layer != 9)
         {
-            touched = true;
-            if (collision.gameObject.GetComponent<Rigidbody2D>().velocity.x < 2f&& collision.gameObject.GetComponent<Rigidbody2D>().velocity.x>-2)
+            if (!touched)
             {
+                touched = true;
+                if (collision.gameObject.GetComponent<Rigidbody2D>().velocity.x < 2f && collision.gameObject.GetComponent<Rigidbody2D>().velocity.x > -2)
+                {
 
-                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-                GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-                GetComponent<BoxCollider2D>().enabled = false;
+                    GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+                    GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                    GetComponent<BoxCollider2D>().enabled = false;
 
-                Door1.SetActive(false);
-                Door2.SetActive(true);
+                    Door1.SetActive(false);
+                    Door2.SetActive(true);
+                }
+                else 
+                { 
+                    GetComponent<BoxCollider2D>().isTrigger = false;
+                    GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                }
             }
         }
     }
---
+++ Assets/Tutorial.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    bool D = false;
+    bool A = false;
+    bool W = false;
+    bool S = false;
+    bool E = false;
+    int timer=0;
+    bool canpress=false;
+    public TextMesh txt;
+    string story;
+
+    void Awake()
+    {
+        txt = txt.GetComponent<TextMesh>();
+        story = txt.text;
+        txt.text = "";
+    }
+
+    IEnumerator PlayText()
+    {
+        canpress = false;
+        txt.text = "";
+        foreach (char c in story)
+        {
+            txt.text += c;
+            yield return new WaitForSeconds(0.025f);
+        }
+        canpress = true;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < 100) timer++;
+        else
+        {
+            if (timer == 100)
+            {
+                timer++;
+                story = "Ok! It's works.\nNow use D to go right";
+                StartCoroutine("PlayText");
+            }
+            else
+            {
+                if (canpress&&Input.GetKeyDown(KeyCode.D) && !D && !A && !W && !S)
+                {
+                    story = "Ok! It's works.\nNow use D to go right";
+                    StartCoroutine("PlayText");
+                    D = true;
+                }
+            }
+        } 
+    }
+}
+
