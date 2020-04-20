fdf99187501aae9cd6df4408b2241fde5e4e08a3
filgreen3 <11trr31@gmail.com>
2020-04-20 17:15:44 +0300

    sound

---
+++ Assets/Code/World/SoundPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundPlayer : MonoBehaviour
+{
+    [SerializeField] protected AudioSource audioSource;
+    [SerializeField] protected AudioClip сlip;
+
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound()
+    {
+        audioSource.pitch = Random.Range(.7f, 1.5f);
+        audioSource.PlayOneShot(сlip);
+    }
+
+
+
+}
