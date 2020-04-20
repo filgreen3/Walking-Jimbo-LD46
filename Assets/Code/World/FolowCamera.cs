fd5efe40f47991082ffe2abe4aaf8c6d3adfd428
fememism <git@koro.games>
2020-04-20 13:48:30 +0300

    конец игры

--- Assets/Code/World/FolowCamera.cs
+++ Assets/Code/World/FolowCamera.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 public class FolowCamera : MonoBehaviour
 {
 
-    private float lerpTime = .05f;
+    private Vector3 Offset;
+    [SerializeField] private float mul = default;
+
+    [SerializeField] private float downBorder = 7;
+    [SerializeField] private float upBorder = 20;
+
+    private float lerpTime=.05f;
+
+    public static Vector3 zeroPoint = Vector3.zero;
+
     public Transform objectToFolow;
     Elevator elevator;
     private void Start()
@@ -14,7 +23,12 @@ public class FolowCamera : MonoBehaviour
     }
     void Update()
     {
-        if (elevator.Level < 1)
+
+        /*var pos = Vector3.Lerp(transform.position, objectToFolow.position + Offset, 0.55f);
+        pos.z = -10;
+        pos.y = pos.y < downBorder ? downBorder : pos.y > upBorder ? upBorder : pos.y;
+        transform.position = pos;*/
+        if (elevator.Level < 0)
         {
             transform.position = Vector3.Lerp(transform.position, objectToFolow.position, lerpTime);
             transform.position = new Vector3(transform.position.x, transform.position.y, -10);
--- Assets/Level_text.cs
+++ Assets/Level_text.cs
@@ -1,13 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Level_text : MonoBehaviour
 {
     Elevator Elevator;
+    bool end = false;
+    public GameObject EndGameSquare;
+    public Text txt;
+    string story;
+    public AudioClip sound;
+    public AudioSource audio;
     void Start()
     {
         Elevator = GameObject.FindGameObjectWithTag("Elevator").GetComponent<Elevator>();
         GetComponent<TextMesh>().text = Elevator.Level.ToString();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Player")
+        {
+            end = true;
+            EndGameSquare.SetActive(true);
+            txt = GameObject.Find("EndGameText").GetComponent<Text>();
+            StartCoroutine("EndGame");
+        }
+    }
+    IEnumerator EndGame()
+    {
+        yield return new WaitForSeconds(2f);
+
+        story = "\n\nThe game was made in 72 hours\nfor Ludum Dare #46";
+        txt.text = "";
+        foreach (char c in story)
+        {
+            txt.text += c;
+            audio.PlayOneShot(sound);
+            audio.pitch = Random.Range(1, 2.1f);
+            yield return new WaitForSeconds(0.025f);
+        }
+        yield return new WaitForSeconds(5f);
+
+
+        story = "\n\nVisit our website for more stuff:\nkoro.games";
+        txt.text = "";
+        foreach (char c in story)
+        {
+            txt.text += c;
+            audio.PlayOneShot(sound);
+            audio.pitch = Random.Range(1, 2.1f);
+            yield return new WaitForSeconds(0.025f);
+        }
+        yield return new WaitForSeconds(10f);
+        SceneManager.LoadScene(0);
+
     }
 }
