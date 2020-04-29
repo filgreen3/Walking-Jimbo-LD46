1634ea8ed24763f1fc61596d0739e197d083b59b
fememism <git@koro.games>
2020-04-29 15:40:10 +0300

    звуки

--- Assets/Code/Weapon/Сudgel.cs
+++ Assets/Code/Weapon/Сudgel.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Сudgel : GenericItem
 {
     bool isHitEnd = true;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (isHitEnd && other.tag == "Player")
         {
             isHitEnd = false;
             other.GetComponent<Player>().GetDamage(0.1f);
+            other.GetComponent<SoundPlayer>().PlaySound();
+
             Invoke("EndHit", 1f);
         }
     }
