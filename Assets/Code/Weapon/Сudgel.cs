e86dab953507e753197023d49f53402e30d07267
filgreen3 <11trr31@gmail.com>
2020-04-29 13:46:20 +0300

    fix-move

--- Assets/Code/Weapon/Сudgel.cs
+++ Assets/Code/Weapon/Сudgel.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Сudgel : GenericItem
 {
-    bool isHitEnd;
+    bool isHitEnd = true;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (isHitEnd && other.tag == "Player")
